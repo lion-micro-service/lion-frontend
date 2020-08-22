a2cf4c50788c4ecfb0e2ca165738a14c8ca085e0
mr.liu <158442534@qq.com>
2020-08-22 16:55:28 +0800

    实现列表加载

--- lion-front-component-frame/src/test/views/List.vue
+++ lion-front-component-frame/src/test/views/List.vue
@@ -4,91 +4,126 @@
             <a-card class="card" :bordered="false">
                 <a-row :gutter="40">
                     <a-col :xs="8">
-                        <a-form-model-item prop="name" ref="name" style="margin-bottom: 0px;">
+                        <a-form-model-item prop="name" ref="name" >
                             <a-input addonBefore="姓名:" placeholder="请输入" v-model="searchModel.name" />
                         </a-form-model-item>
                     </a-col>
                     <a-col :xs="8">
-                        <a-form-model-item prop="name" ref="name" style="margin-bottom: 0px;">
+                        <a-form-model-item prop="name" ref="name" >
                             <a-input addonBefore="姓名:" placeholder="请输入" v-model="searchModel.name" />
                         </a-form-model-item>
                     </a-col>
                     <a-col :xs="8">
-                        <a-form-model-item prop="name" ref="name" style="margin-bottom: 0px;">
+                        <a-form-model-item prop="name" ref="name" >
                             <a-input addonBefore="姓名:" placeholder="请输入" v-model="searchModel.name" />
                         </a-form-model-item>
                     </a-col>
                 </a-row>
                 <a-row :gutter="40">
                     <a-col :xs="8">
-                        <a-form-model-item prop="name" ref="name" style="margin-bottom: 0px;">
+                        <a-form-model-item prop="name" ref="name" >
                             <a-input addonBefore="姓名:" placeholder="请输入" v-model="searchModel.name" />
                         </a-form-model-item>
                     </a-col>
                     <a-col :xs="8">
-                        <a-form-model-item prop="name" ref="name" style="margin-bottom: 0px;">
+                        <a-form-model-item prop="name" ref="name" >
                             <a-input addonBefore="姓名:" placeholder="请输入" v-model="searchModel.name" />
                         </a-form-model-item>
                     </a-col>
                     <a-col :xs="8">
-                        <a-form-model-item prop="name" ref="name" style="margin-bottom: 0px;">
+                        <a-form-model-item prop="name" ref="name" >
                             <a-input addonBefore="姓名:" placeholder="请输入" v-model="searchModel.name" />
                         </a-form-model-item>
                     </a-col>
                 </a-row>
 
-                <a-form-item style="text-align:right;margin-bottom: 0px;">
-                    <a-button type="primary" icon="search">查询</a-button>
-                    <a-button type="primary">添加</a-button>
-                    <a-button type="danger">删除</a-button>
+                <a-form-item style="text-align:right;">
+                    <a-button type="primary" icon="search" @click="search()">查询</a-button>
+                    <a-button type="primary" icon="file-add">添加</a-button>
+                    <a-button type="danger" icon="delete">删除</a-button>
                 </a-form-item>
             </a-card>
         </a-form-model>
 
-        <a-table :columns="columns" :dataSource="data">
-            <a slot="action" slot-scope="text">action</a>
-        </a-table>
+        <a-card :bordered="false">
+            <a-table rowKey="id" :columns="columns" :dataSource="data" :loading="loading" :pagination="paginationProps">
+
+            </a-table>
+        </a-card>
     </div>
 </template>
 
 <script lang="ts">
     import {Component, Emit, Inject, Model, Prop, Provide, Vue, Watch} from 'vue-property-decorator';
+    import axios from "@lion/lion-front-core/src/network/axios";
     @Component({})
     export default class List extends Vue{
 
         private searchModel : any ={
-            name : "测试"
+            name : "",
+            pageNumber:1,
+            pageSize:1
         }
 
-        private x : number = 1500;
-        private y : number = 300;
+        private data:Array<any> = [];
+        private loading:boolean=false;
+        //private x : number = 1500;
+        //private y : number = 300;
 
         private columns :Array<any> = [
-            { title: 'Full Name', width: 100, dataIndex: 'name', key: 'name', fixed: 'left' },
-            { title: 'Age', width: 100, dataIndex: 'age', key: 'age', fixed: 'left' },
-            { title: 'Column 1', dataIndex: 'address', key: '1', width: 150 },
-            { title: 'Column 2', dataIndex: 'address', key: '2', width: 150 },
-            { title: 'Column 3', dataIndex: 'address', key: '3', width: 150 },
-            { title: 'Column 4', dataIndex: 'address', key: '4', width: 150 },
-            { title: 'Column 5', dataIndex: 'address', key: '5', width: 150 },
-            { title: 'Column 6', dataIndex: 'address', key: '6', width: 150 },
-            { title: 'Column 7', dataIndex: 'address', key: '7', width: 150 },
-            { title: 'Column 8', dataIndex: 'address', key: '8' },
-            {
-                title: 'Action',
-                key: 'operation',
-                fixed: 'right',
-                width: 100,
-                scopedSlots: { customRender: 'action' },
-            },
+            { title: '姓名', dataIndex: 'name', key: 'name' },
+            { title: '邮箱', dataIndex: 'email', key: 'email'},
+            { title: '年龄', dataIndex: 'age', key: 'age' }
         ];
 
-        private data:Array<any> = [{key:1,name:'Edrward',age:32,address:'London Park no'},
-            {key:2,name:'ss',age:32,address:'London Park no'}];
+        private paginationProps:any={
+            showSizeChanger: true,
+            showQuickJumper: true,
+            current:this.searchModel.pageNumber,
+            pageSize:this.searchModel.pageSize,
+            hideOnSinglePage:false,
+            pageSizeOptions:['10', '20', '30', '40','50','60','70','80','90','100'],
+            showSizeChange: (pageNumber:number, pageSize: number)=>this.paginationSearch(pageNumber,pageSize),
+            onChange: (pageNumber:number, pageSize: number)=>this.paginationSearch(pageNumber,pageSize),
+        };
+
+        private mounted() :void{
+            this.search();
+        }
+
+        private paginationSearch(pageNumber:number, pageSize: number):void{
+            this.searchModel.pageNumber=pageNumber;
+            this.searchModel.pageSize=pageSize;
+            this.search();
+        }
+
+        private search():void{
+            this.loading=true;
+            axios.get("/upms/user/console/list",{params:this.searchModel})
+                .then((data)=>{
+                        this.data=data.data.list;
+                        this.paginationProps.total=(Object(data)).totalElements;
+                        this.paginationProps.current=(Object(data)).pageNumber;
+                        this.paginationProps.pageSize=(Object(data)).pageSize;
+                    }
+                )
+                .catch(fail => {
+
+                    }
+                )
+                .finally(()=>{
+                        this.loading=false;
+                    }
+                );
+        }
+
+
 
     }
 </script>
 
 <style lang="less" scoped>
-
+    .ant-form-item{
+        margin-bottom: 0px;
+    }
 </style>
