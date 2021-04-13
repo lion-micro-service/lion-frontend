d44d7c67f02ea1da103e35d58a14df568c3d2a22
mr.liu <158442534@qq.com>
2021-04-13 13:34:28 +0800

    优化菜单

--- lion-front-component-frame/src/404/index.ts
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue';
-import index from './Index.vue';
-import store from './store/index';
-import router from "./router/index";
-new Vue({
-    store,
-    router,
-    render:h=>h(index)
-}).$mount("#app");
--- lion-front-core/src/index.js
+++
@@ -1,9 +0,0 @@
-let pages = require('./webpack/vue.config.js')
-let store = require('./store/index.ts')
-
-exports.pages = pages;
-exports.store = store;
-// export {
-//     pages,
-//     store
-// }
